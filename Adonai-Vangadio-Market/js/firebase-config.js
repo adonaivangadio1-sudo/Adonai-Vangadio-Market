43434ffe50b56cfe0ad1e56cbe2267cceeeadc1a
Adonai Vangadio <adonaivangadio1@gmail.com>
2026-08-11 08:44:56 +0100

    Add Firebase configuration and initialization
    This file contains the Firebase configuration, initialization, and exports for authentication and Firestore.

---
+++ Adonai-Vangadio-Market/js/firebase-config.js
@@ -0,0 +1,83 @@
+/* ======================================================
+   AV MARKET
+   FIREBASE CONFIGURATION
+====================================================== */
+
+import {
+    initializeApp,
+    getApps,
+    getApp
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+/* ======================================================
+   CONFIGURAÇÃO FIREBASE
+====================================================== */
+
+const firebaseConfig = {
+
+    apiKey:
+        "AIzaSyA1YgIK05tzyP7963pQgUeEY2US6bHV52o",
+
+    authDomain:
+        "av-market-a39b6.firebaseapp.com",
+
+    projectId:
+        "av-market-a39b6",
+
+    storageBucket:
+        "av-market-a39b6.firebasestorage.app",
+
+    messagingSenderId:
+        "211922806104",
+
+    appId:
+        "1:211922806104:web:cfa19f4583e0cb4fa1712d"
+
+};
+
+
+/* ======================================================
+   INICIALIZAÇÃO DO FIREBASE
+   Evita initializeApp() duplicado
+====================================================== */
+
+const app =
+    getApps().length > 0
+        ? getApp()
+        : initializeApp(firebaseConfig);
+
+
+/* ======================================================
+   FIREBASE AUTHENTICATION
+====================================================== */
+
+const auth =
+    getAuth(app);
+
+
+/* ======================================================
+   FIRESTORE
+====================================================== */
+
+const db =
+    getFirestore(app);
+
+
+/* ======================================================
+   EXPORTAR
+====================================================== */
+
+export {
+    app,
+    auth,
+    db
+};
